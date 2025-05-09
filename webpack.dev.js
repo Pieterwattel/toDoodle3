cc17b4af980580e9000cdec913a69768b012ab2a
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-05-09 19:14:44 +0200

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html", "./src/styles.css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+});
