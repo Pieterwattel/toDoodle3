61bd72b9aaa0af9d2456f24972e12925b538a6da
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-05-13 11:23:54 +0200

    clean up todoItem a little bit
    I want more data to be taken care of in the constructor.
    This is a first step, I will continuw with it later

--- src/model/todoItem.js
+++ src/model/todoItem.js
@@ -1,39 +1,53 @@
+import { dates } from './dates.js';
+
 class TodoItem {
-    #id
-    #title
-    #importance
-    #lastDayOfDeadline
-    #dateSpecifiedByUser
-    #finished
+  #id;
+  #title;
+  #importance;
+  #lastDayOfDeadline;
+  #dateSpecifiedByUser;
+  #finished;
 
-    constructor(dataObj) {
-        this.#id = dataObj.id;
-        this.#title = dataObj.title;
-        this.#importance = dataObj.importance;
+  constructor(dataObj) {
+    this.#id = TodoItem.id;
+    this.#title = dataObj.title;
+    this.#importance = dataObj.importance;
 
-        this.#lastDayOfDeadline = dataObj.lastDayOfDeadline;
-        this.#dateSpecifiedByUser = dataObj.dateSpecifiedByUser;
+    this.#lastDayOfDeadline = dataObj.lastDayOfDeadline;
+    this.#dateSpecifiedByUser = dataObj.dateSpecifiedByUser;
 
-        this.#finished = dataObj.finished;
-    }
+    this.#finished = dataObj.finished ? foo : (dataObj.finished = false);
+
+    TodoItem.id++;
+  }
 
   static id = 0;
 
   static create(dataObj) {
-    const newDataObj = this.getAdditionalCreationData(dataObj)
+    const newDataObj = this.getAdditionalCreationData(dataObj);
     const newTodo = new TodoItem(newDataObj);
     return newTodo;
   }
 
   store(todo) {
     storage.array.push(todo);
-  } 
+  }
 
-  static getAdditionalCreationData(todoDataObj) {
-    todoDataObj.id = this.id++;
-    todoDataObj.finished ? foo : todoDataObj.finished = false
-    return todoDataObj
+  static getAdditionalCreationData(dataObj) {
+    if (dataObj.lastDayOfDeadline) {
+      dataObj.lastDayOfDeadline = dates.parseHtmlDateToFnsFormat(
+        dataObj.lastDayOfDeadline,
+      );
+      dataObj.dateSpecifiedByUser = true;
+    } else {
+      console.log(dates.getDeadlineDependingOnUrgency(dataObj.urgency));
+      dataObj.lastDayOfDeadline = dates.getDeadlineDependingOnUrgency(
+        dataObj.urgency,
+      );
+      dataObj.dateSpecifiedByUser = false;
+    }
+    return dataObj;
   }
 }
 
-export {TodoItem}
+export { TodoItem };
