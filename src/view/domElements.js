219927ef7379c9517b95872c98db15926cacbe0a
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-05-19 12:10:41 +0200

    do some basic fronend scaffolding
    I made some html input for the data,
    and connected these to JS nodes

---
+++ src/view/domElements.js
@@ -0,0 +1,17 @@
+let domElements = {
+  todoCreationInputs: {
+    titleInput: document.getElementById('titleInput'),
+    importanceSelector: document.getElementById('importanceSelector'),
+    urgencySelector: document.getElementById('urgencySelector'),
+    urgencyDateInput: document.getElementById('urgencyDateInput'),
+    categorySelector: document.getElementById('categorySelector'),
+    categoryInput: document.getElementById('categoryInput'),
+    descriptionInput: document.getElementById('descriptionInput'),
+  },
+  //
+  todoCreationBtn: document.getElementById('todoCreationBtn'),
+
+  //
+};
+
+export { domElements };
