dbb2a126d4f39fa445173c19d2c2d623f8063891
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-05-21 17:52:20 +0200

    commit to be able to deploy github pages
    I am trying, its not working,
    I am worried webpack might be in the way.

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,25 @@
 // webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,22 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
   devServer: {
-    watchFiles: ["./src/index.html", "./src/styles.css"],
+    watchFiles: ['./src/index.html', './src/styles.css'],
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
